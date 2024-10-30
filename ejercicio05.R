77840e77c624577917c7a2d51ad9f43b6fb831ce
Nat1310 <natmartinezca@unal.edu.co>
2024-10-30 18:04:38 -0500

    Update ejercicio05.R

--- ejercicio05.R
+++ ejercicio05.R
@@ -2,8 +2,8 @@
 
 solucion <- function() {
   # Ordena (sin realizar nuevas asignaciones) de manera que se devuelva un valor de 's' diferente a NULL
-  a <- 10
-  b <- 20
+  a <- 20
+  b <- 10
   s <- a+b
   s <- NULL
   return(s) 
